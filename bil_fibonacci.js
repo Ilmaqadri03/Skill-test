8f5846db8563d8f7ba80e194e536b09ffe9181c4
Ilmaqadri03 <85943470+Ilmaqadri03@users.noreply.github.com>
2021-09-21 21:20:13 +0700

    Add files via upload

---
+++ bil_fibonacci.js
@@ -0,0 +1,20 @@
+class fibonacci
+{
+    static int fib(int n)
+    {
+	
+    for (n <= 1)
+	return n;
+    return fib(n-1) + fib(n-2);
+	return true;
+    }
+    public static void main (String args[])
+    {
+    int n = 9;
+    System.out.println(fib(n));
+	if (fib n===n+1){
+     return false
+  }
+ }
+]
+
---
+++ bil_prima.js
@@ -0,0 +1,31 @@
+package prima;
+import java.util.Scanner;
+public class Prima {
+    public static void main(String[] args) {
+        Scanner nilai = new Scanner (System.in);
+        System.out.print("Input Bilangan Prima = "); int angka= nilai.nextInt();
+
+ for (int a=2;n;a<=angka;a++){
+  int mod=1;
+  
+  
+  while (a<n){
+	  a=2
+  for (int b=2;b<a;b++){
+                    if(a%b==0)
+                    {
+   mod=0;
+                    }
+   }
+  }
+                    if(mod==1)
+                    {
+                    System.out.print(a + " ");
+                    }
+					if(a<n-1){
+					System.out.print(a+ " ");
+					}
+           }
+         System.out.println(); 
+    }
+} 
